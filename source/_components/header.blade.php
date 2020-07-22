5f407e71cf6c8e6f4422b5d735238195fe49bba5
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-07-22 12:10:06 -0700

    Extracted search to a dedicated component

--- source/_components/header.blade.php
+++ source/_components/header.blade.php
@@ -7,18 +7,5 @@
         Discover the day you joined Reddit!
     </h2>
 
-    <form action="." method="GET" v-on:submit.prevent="submit(search)"
-        class="group relative bg-white rounded-full shadow-inner px-10 py-2 mt-6"
-    >
-        <input type="text" name="username" placeholder="Reddit username" v-model="search" ref="searchInput" v-on:focus="$event.target.select()">
-
-        <div class="flex justify-center items-center absolute left-0 inset-y-0 ml-2 pointer-events-none">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="#a0aec0" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z"/>
-                <circle cx="10" cy="10" r="7" />
-                <line x1="21" y1="21" x2="15" y2="15" />
-                <line x1="21" y1="21" x2="17" y2="17" stroke-width="3" />
-            </svg>
-        </div>
-    </form>
+    @include('_components.search')
 </div>
