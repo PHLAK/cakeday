d0822e62c87d5b99eb78894c78eb96e060296dc1
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-07-22 12:34:56 -0700

    Linked title to home page

--- source/_components/header.blade.php
+++ source/_components/header.blade.php
@@ -1,7 +1,9 @@
 <div class="text-center">
-    <h1 class="font-light text-white text-4xl">
-        <i class="fas fa-birthday-cake fa-fw text-reddit"></i> Reddit Cake Day
-    </h1>
+    <a href="/">
+        <h1 class="font-light text-white text-4xl">
+            <i class="fas fa-birthday-cake fa-fw text-reddit"></i> Reddit Cake Day
+        </h1>
+    </a>
 
     <h2 class="font-light text-base text-gray-500">
         Discover the day you joined Reddit!
